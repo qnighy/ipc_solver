e7d5da36e5ed2fff8764a75254b71d24c1fe7125
Masaki Hara <ackie.h.gmai@gmail.com>
2013-10-21 01:34:40 +0900

    add reference to the paper

--- solver.ml
+++ solver.ml
@@ -1,3 +1,13 @@
+(*
+ * This solver is based on:
+ * JÖRG HUDELMAIER,
+ * An O(n log n)-Space Decision Procedure
+ * for Intuitionistic Propositional Logic.
+ * J Logic Computation (1993) 3 (1): 63-75.
+ * DOI: http://dx.doi.org/10.1093/logcom/3.1.63
+ *)
+(* author: Masaki Hara *)
+
 open Term
 open Lf_proof
 
