fd1b8418476afb7d4df942cf0226452776bd8c48
Masaki Hara <ackie.h.gmai@gmail.com>
2013-10-22 23:40:18 +0900

    modify make-image

--- make-image.sh
+++ make-image.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
-ulimit -t 10
-ulimit -f 300000
-ulimit -l 512000
+#!/bin/bash
+ulimit -St 10
+ulimit -Sf 300000
+ulimit -Sv 512000
 ./ipc_solver > $1.tex && \
-pdflatex $1.tex -output-directory `dirname $1` && \
+pdflatex -halt-on-error $1.tex -output-directory `dirname $1` && \
 convert -density 300 $1.pdf -quality 90 -limit area 0 -resize '4000>' \
   -fill white -opaque none -alpha off $1.png
