02a9ae5e7ceedc3f623b81adf062400977ca5be0
Masaki Hara <ackie.h.gmai@gmail.com>
2016-10-09 11:47:59 +0900

    Introduce latex output options.

--- main.ml
+++ main.ml
@@ -1,11 +1,22 @@
 open Term
 open Format
 
+let latex_output = ref None
+
+let usage_msg =
+  "ipc_solver is a solver for intuitionistic propositional formulas.\n" ^
+  "\n" ^
+  "usage:"
+
 let speclist = [
+  ("--latex", Arg.String (fun path -> latex_output := Some path),
+    "Sets a path for latex output");
+  ("--no-latex", Arg.Unit (fun _ -> latex_output := None),
+    "Cancels latex output")
 ]
 
 let () =
-  Arg.parse speclist (fun _ -> ()) "usage:";
+  Arg.parse speclist (fun _ -> ()) usage_msg;
   let lexbuf = Lexing.from_channel stdin in
   begin try
     let tn = Parser.main Lexer.token lexbuf in
@@ -15,43 +26,59 @@ let () =
     begin match Solver.solve num t with
     | Some pr ->
         eprintf "solved. provable@.";
-        (* eprintf "proof(LF, plain): %a@."
-          Lf_proof.pp_print_proofitem pr; *)
-        (* eprintf "proof(LF):@,%a@."
-          (Lf_proof.pp_print_proof env num t) pr; *)
-        let npr = Nj_proof.convert_lf t pr in
-        (* eprintf "proof(NJ):@,%a@."
-          (Nj_proof.pp_print_lambda env) npr; *)
-        ignore (Nj_proof.nj_check_type [] npr);
-        let npr = Nj_proof.postproc_proof npr in
-        ignore (Nj_proof.nj_check_type [] npr);
-        (* eprintf "proof(NJ):@,%a@."
-          (Nj_proof.pp_print_lambda env) npr; *)
+        begin match !latex_output with
+        | Some latex_path ->
+            (* eprintf "proof(LF, plain): %a@."
+              Lf_proof.pp_print_proofitem pr; *)
+            (* eprintf "proof(LF):@,%a@."
+              (Lf_proof.pp_print_proof env num t) pr; *)
+            let npr = Nj_proof.convert_lf t pr in
+            (* eprintf "proof(NJ):@,%a@."
+              (Nj_proof.pp_print_lambda env) npr; *)
+            ignore (Nj_proof.nj_check_type [] npr);
+            let npr = Nj_proof.postproc_proof npr in
+            ignore (Nj_proof.nj_check_type [] npr);
+            (* eprintf "proof(NJ):@,%a@."
+              (Nj_proof.pp_print_lambda env) npr; *)
 
-        (* printf "%s@." "\\documentclass[preview]{standalone}"; *)
-        printf "%s@." "%provable";
-        printf "%s@." "\\documentclass[preview,varwidth=10000px,12pt]{standalone}";
-        printf "%s@." "\\usepackage{bussproofs}";
-        printf "%s@." "\\usepackage{color}";
-        printf "%s@." "\\begin{document}";
-        printf "%a@."
-          (Nj_proof.print_nj_latex env) npr;
-        printf "%s@." "\\end{document}"
+            (* printf "%s@." "\\documentclass[preview]{standalone}"; *)
+            let f = open_out latex_path in
+            let ff = formatter_of_out_channel f in
+            fprintf ff "%s@." "%provable";
+            fprintf ff "%s@." "\\documentclass[preview,varwidth=10000px,12pt]{standalone}";
+            fprintf ff "%s@." "\\usepackage{bussproofs}";
+            fprintf ff "%s@." "\\usepackage{color}";
+            fprintf ff "%s@." "\\begin{document}";
+            fprintf ff "%a@." (Nj_proof.print_nj_latex env) npr;
+            fprintf ff "%s@." "\\end{document}";
+            close_out f
+        | None -> ()
+        end
     | None ->
         eprintf "solved. unprovable@.";
-        printf "%s@." "%unprovable";
-        printf "%s@." "\\documentclass[preview,varwidth=4000px]{standalone}";
-        printf "%s@." "\\begin{document}";
-        printf "%s@." "unprovable";
-        printf "%s@." "\\end{document}"
+        begin match !latex_output with
+        | Some latex_path ->
+            let f = open_out latex_path in
+            let ff = formatter_of_out_channel f in
+            fprintf ff "%s@." "%unprovable";
+            fprintf ff "%s@." "\\documentclass[preview,varwidth=4000px]{standalone}";
+            fprintf ff "%s@." "\\begin{document}";
+            fprintf ff "%s@." "unprovable";
+            fprintf ff "%s@." "\\end{document}"
+        | None -> ()
+        end
     end
   with
   | Parsing.Parse_error ->
       eprintf "Parse Error@.";
-      printf "%s@." "%parse_error";
-      printf "%s@." "\\documentclass[preview,varwidth=4000px]{standalone}";
-      printf "%s@." "\\begin{document}";
-      printf "%s@." "Parse Error";
-      printf "%s@." "\\end{document}"
+      begin match !latex_output with
+      | Some latex_path ->
+          printf "%s@." "%parse_error";
+          printf "%s@." "\\documentclass[preview,varwidth=4000px]{standalone}";
+          printf "%s@." "\\begin{document}";
+          printf "%s@." "Parse Error";
+          printf "%s@." "\\end{document}"
+      | None -> ()
+      end
   end
 
--- twitter-make-image.sh
+++ twitter-make-image.sh
@@ -3,6 +3,6 @@ ulimit -St 10
 ulimit -Sf 300000
 ulimit -Sv 512000
 cd workdir && \
-../ipc_solver < $1-prop.txt >$1.tex 2>$1.log && \
+../ipc_solver --latex $1.tex < $1-prop.txt 2>$1.log && \
 latex -halt-on-error $1.tex >>$1.log 2>&1 && \
 dvipng $1.dvi
